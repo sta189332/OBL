884842657fc4a6ff294db0871f046d9075797694
sta189332 <futathesis@gmail.com>
2022-06-14 13:19:20 +0100

    CRAN correction commit

--- R/data.R
+++ R/data.R
@@ -9,5 +9,7 @@
 #'   \item{carat}{weight of the diamond, in carats}
 #'   ...
 #' }
-#' @source \url{(set.seed(289805)ts <- stats::arima.sim(n = 10, model = list(ar = 0.8, order = c(1, 0, 0)), sd = 1)}
+#' @source Simulated data generated with the following code:
+#'   set.seed(289805)
+#'   ts <- stats::arima.sim(n = 10, model = list(ar = 0.8, order = c(1, 0, 0)), sd = 1)
 "ts"
