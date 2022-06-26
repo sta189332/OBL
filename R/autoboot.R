f835efe4035d6d80ce155725c841843b1d29f5da
sta189332 <futathesis@gmail.com>
2022-06-26 15:52:55 +0100

    CRAN-SUBMISSION

--- R/autoboot.R
+++ R/autoboot.R
@@ -35,7 +35,7 @@
 #' @return A data frame get printed to the console
 #'
 #' @examples
-#' #' \dontrun{
+#' #' \donttest{
 #' set.seed(289805)
 #' ts <- arima.sim(n = 10, model = list(ar = 0.8, order = c(1, 0, 0)), sd = 1)
 #' blockboot(ts = ts, R = 100, seed = 6, n_cores = 2)
